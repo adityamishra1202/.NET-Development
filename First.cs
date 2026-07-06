4bcbbcaa20b9e16017824b62291fa95a3b9f2b47
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-06 19:56:31 +0530

    initial commmoit

---
+++ First.cs
@@ -0,0 +1,11 @@
+using System;
+
+class First {
+public void func()
+{
+Console.WriteLine("Name:Aditya Mishra"+"\nClass:3N");
+
+}
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program{
+static void Main(string[] args){
+Console.WriteLine("The student of Shri Sant Gajanan Maharaj College of Engineering");
+First f=new First ();
+f.func();
+}
+}
