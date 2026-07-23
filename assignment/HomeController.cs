d7b5620ecf6f1340784370bc7810ae0503608dd5
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-23 09:36:03 +0530

    22 July Assignment

--- assignment/HomeController.cs
+++ assignment/HomeController.cs
@@ -1,48 +1,57 @@
 using System.Diagnostics;
-using _21_July_Assign.Models;
+using _22_July_Assign.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace _21_July_Assign.Controllers
+namespace _22_July_Assign.Controllers
 {
     public class HomeController : Controller
     {
+
+
 
         public IActionResult Index()
         {
             return View();
         }
 
+
+        //receive form data
+
         [HttpPost]
-        public IActionResult Index(Employee employee)
+        public ActionResult Index(Automobile at)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                TempData["emp"] = employee.Employee_Name;
-                TempData["dep"] = employee.Department;
-                return RedirectToAction("Department");
+                //normally save to db
+                //return Content($"vehicle Name: {at.Vehicle_Name}, " +
+                //    $"Breand: {at.Brand}, " +
+                //    $"Model Year: {at.Model_Year}, " +
+                //    $"Price: {at.Price},"+
+                //    $"Fuel Type:{at.FuelType}");
+                TempData ["Victory"] = "Automobile Registered Successfully";
+                TempData["VehicleName"] = at.Vehicle_Name;
+                TempData["Brand"] = at.Brand;
+                return RedirectToAction("Manufacture");
 
             }
-            return View(employee);
+
+            return View(at);
+
+
+        }
+        public IActionResult Manufacture()
+        {
+            return View();
+        }
+        [HttpPost]
+        
+        public IActionResult Manufacture(Manufacture man)
+        {
+            
+            return View(man);
         }
 
-        public IActionResult Department()
-        {
-            List<Department> departments = new List<Department>()
-            {
-             new Department{Department_Name="HR",Department_Head="Kiran Wadatkar",Contact_No=6788998765,Email="kiran@1234" },
-              new Department{Department_Name="QC",Department_Head="Vedant Tayde",Contact_No=9988998765,Email="vedant@1234" },
-               new Department{Department_Name="Logistics",Department_Head="V.R. Kannan",Contact_No=9988576748,Email="kannan@1234" },
-                new Department{Department_Name="HRM",Department_Head="Sanjay Singh",Contact_No=9878899675,Email="sanjay@1234" },
-                 new Department{Department_Name="CRM",Department_Head="Jay Ghosh",Contact_No=9284775234,Email="jay@1234" },
 
-
-
-
-
-            };
-                return View(departments);
- }
 
     }
-
 }
