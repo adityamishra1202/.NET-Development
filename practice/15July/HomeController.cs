8a330a696264593b94dccea21ec3d80ab7fd57a0
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-25 01:26:02 +0530

    15july

---
+++ practice/15July/HomeController.cs
@@ -0,0 +1,33 @@
+//Brain of MVC
+using System.Diagnostics;
+using _15JULY.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _15JULY.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()//is an action method-every url calls an action method
+        {
+            return View();  //returns a view to the browser
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
