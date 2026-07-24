c460adf2cb800272f90dbafa22aa8fc8894c1bc0
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-25 00:08:59 +0530

    Delete assignment/ErrorViewModel.cs

--- assignment/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _16July_Assign.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
