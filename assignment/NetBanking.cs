a28949899e0b258aaee1e2ea08913f5a93fd397c
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-25 00:17:43 +0530

    Delete assignment/NetBanking.cs

--- assignment/NetBanking.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class NetBanking : PaymentGateway
-{
-    public  void ProcessPayment(int amount)
-    {
-        Console.WriteLine("Amount paid using Net Banking "+ amount);
-    }
-}
