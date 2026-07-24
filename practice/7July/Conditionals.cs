f4915ffc144b14b09f08447ae9cc289b6b4b6173
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-25 00:48:36 +0530

    7July

---
+++ practice/7July/Conditionals.cs
@@ -0,0 +1,17 @@
+using System;
+class Conditionals
+{
+    public void result(){
+    
+
+int a=45;
+if (a > 50)
+{
+    Console.WriteLine("Good");
+}
+else
+{
+    Console.WriteLine("bad");
+}
+    }
+}
