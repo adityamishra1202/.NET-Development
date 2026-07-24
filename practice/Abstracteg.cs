9bb3fc5d2f11ffd5a4f59566089fe65aa8c34b06
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-25 00:03:17 +0530

    Delete practice/Abstracteg.cs

--- practice/Abstracteg.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Abstracteg : FileStorage
-{
-    public override void Upload(string filename)
-    {
-        Console.WriteLine("Uploading file to Google Cloud");
-    }
-}
