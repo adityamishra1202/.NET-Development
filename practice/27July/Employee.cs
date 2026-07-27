8e19ace3b107bc8ffb576cbd4ad15fea7494766a
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-27 23:40:58 +0530

    27July

---
+++ practice/27July/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _27_July.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Emp id is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Emp name is required")]
+        [StringLength(25,MinimumLength=3,ErrorMessage ="The dept name cannot exceed above the 25 letters")]
+        public string Name { get; set; }
+        
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Emp dept is required")]
+        [StringLength(25,ErrorMessage = "The dept name cannot exceed above the 25 letters")]
+        public string Dept {  get; set; }
+
+        [Required(ErrorMessage = "Emp phone number is required")]
+        public long PhoneNum {  get; set; } 
+
+        public string Location {  get; set; }   
+
+        public string Profile { get; set; }
+    }
+}
