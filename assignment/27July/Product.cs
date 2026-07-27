f655f6a8782a084ba0a4dd670b2716d2a0b4d7b8
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-27 23:44:17 +0530

    27July

---
+++ assignment/27July/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace _27_July_Assign.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Product_Name {  get; set; }
+        public int price {  get; set; }  
+
+        public string Category {  get; set; }
+
+        public int Stock { get; set; }  
+    }
+}
---
+++ assignment/27July/ProductController.cs
@@ -0,0 +1,112 @@
+﻿using _27_July_Assign.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _27_July_Assign.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>() { 
+        
+        new Product{Id=101,Product_Name="Biscuit",price=50,Category="Grocery",Stock=50},
+        new Product{Id=102,Product_Name="Pen",price=50,Category="Stationary",Stock=500},
+        new Product{Id=116,Product_Name="Detergent",price=300,Category="Cleaning agents",Stock=90},
+         new Product{Id=111,Product_Name="Namkeen",price=350,Category="Grocery",Stock=200},
+                  new Product{Id=109,Product_Name="Notebook",price=1150,Category="Stationary",Stock=30},
+
+ };
+
+
+        [HttpGet]
+        public ActionResult getProduct()
+        {
+            return Ok(products);
+        }
+        [HttpGet("{Id}")]
+        public ActionResult getProductById(int id)
+        {
+            var product=products.FirstOrDefault(x => x.Id == id);
+            if(product == null)
+            {
+                return BadRequest();
+            }
+           
+            return Ok(product);         
+
+        }
+        [HttpPut("{id}")]
+        public ActionResult UpdateProduct(int id,Product pro)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            product.price = pro.price;
+            
+
+            return Ok(product);
+
+        }
+        [HttpPost]
+        public ActionResult AddProduct( Product pro)
+        {
+            products.Add(pro);
+            return CreatedAtAction(nameof(getProduct), new { Id = pro.Id },pro);
+
+        }
+
+
+        [HttpDelete("{id}")]
+        
+        public ActionResult DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            products.Remove(product);
+            return NoContent();
+
+            }
+
+
+
+        [HttpGet("Category/{category}")]
+        public ActionResult getProductByCategory(string category)
+        {
+            var result = products.Where(s => s.Category.Equals(category, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (!result.Any()) { return NotFound("Not employee found under this dept"); }
+            return Ok(result);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
