6f193def141ec876d5617bda2f6f4885df9c7428
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-29 23:51:49 +0530

    28Jyly

---
+++ practice/28July/Product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.MicrosoftExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace _28July.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Product name must be between 3 to 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Product price is required")]
+        [Range(10, 100000, ErrorMessage = "Product price must be between 10 to 100000")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Product quantity is required")]
+        [Range(10, 100, ErrorMessage = "Product quantity must be between 10 to 100")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ practice/28July/Program.cs
@@ -0,0 +1,25 @@
+using _28July.Services;
+using _28July.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
