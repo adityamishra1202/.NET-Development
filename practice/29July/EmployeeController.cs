b443bfa7856aaf82b45da3a01373f36fdcdcbcd4
adityamishra1202 <mishraaditya1202@gmail.com>
2026-07-29 23:55:56 +0530

    29July

---
+++ practice/29July/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using _29_July.Models;
+using _29_July.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _29_July.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _service;   // read only service variable
+
+        public EmployeeController(IEmployeeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_service.getEmployees());
+        }
+
+        [HttpGet("{deptid}")]
+        public IActionResult GetById(int deptid)
+        {
+            var employee = _service.getEmployeee(deptid);
+
+            if (employee == null)
+            {
+                return NotFound("Employee with id not found");
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpGet("Name/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var employee = _service.getEmployeeName(name);
+
+            if (employee == null)
+            {
+                return NotFound("Employee with name not found");
+            }
+
+            return Ok(employee);
+        }
+        [HttpPost]
+        public IActionResult Post(Employee employee)
+        {
+            var res = _service.addEmployee(employee);
+
+            return Ok(res);
+        }
+    }
+}
---
+++ practice/29July/IVehicleService.cs
@@ -0,0 +1,14 @@
+﻿using _29_July.Models;
+
+namespace _29_July.Services
+{
+    public interface IVehicleService
+    {
+        List<Vehicle> GetVehicles();
+        Vehicle AddVehicle(Vehicle vehicle);
+
+        Vehicle UpdateVehicle(int id, Vehicle vehicle);
+        List<Vehicle> GetVehicleByWheels(int wheels); 
+
+    }
+}
