f6434b09bf92851eb388f0a99f3156722b386a48
adityamishra1202 <mishraaditya1202@gmail.com>
2026-08-09 17:47:24 +0530

    4Aug

---
+++ practice/4Aug/IStudentService.cs
@@ -0,0 +1,20 @@
+﻿using _4_Aug.Models;
+
+namespace _4_Aug.Repository
+{
+    public interface IStudentService
+    {
+        
+       
+            List<Student> GetAll();
+
+            Student? GetStudent(int id);
+
+            void AddStudent(Student student);
+
+            void UpdateStudent(Student student);
+
+            void DeleteStudent(int id);
+  
+}
+}
