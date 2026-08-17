726c2bc3aa48fa435ca56c3ba7c42bd1c781dd91
adityamishra1202 <mishraaditya1202@gmail.com>
2026-08-17 09:21:55 +0530

    10Aug

---
+++ assignment/10July/ExceptionMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace _10_Aug_Assign.GlobalException
+{
+    public class ExceptionMiddleware
+    {
+      
+            private readonly RequestDelegate next;
+
+            public ExceptionMiddleware(RequestDelegate next)
+            {
+                this.next = next;
+            }
+
+            public async Task Invoke(HttpContext context)
+            {
+                try
+                {
+                    await next(context);
+                }
+                catch (Exception ex)
+                {
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync(
+                        $"Error: {ex.Message}"
+                    );
+                }
+            }
+        }
+    }
+
